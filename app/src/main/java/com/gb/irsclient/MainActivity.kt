b3a8f7c8aece7fba639e626fbf78c354cf0f54de
GreenLight <1251191746@qq.com>
2019-04-09 14:13:27 +0800

    客户端添加发送命令功能
    注意对本地服务器进行设置以适配选择的功能，否则可能会导致服务器崩溃（如本地服务器运行命令，但客户端去发送了文件过去）这个后续会进行优化，将客户端选择的功能信息一并发送到本地服务器中，本地服务在进行分析处理

--- app/src/main/java/com/gb/irsclient/MainActivity.kt
+++ app/src/main/java/com/gb/irsclient/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import me.rosuh.filepicker.config.FilePickerManager
@@ -15,6 +16,7 @@ import java.net.Socket
 class MainActivity : AppCompatActivity() {
 
     var isSelectFile = false
+    var isRunCommand = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,26 +31,32 @@ class MainActivity : AppCompatActivity() {
             if (localServerNameEdt.text.isEmpty()) {
                 Toast.makeText(this, "本地服务器为空", Toast.LENGTH_SHORT).show()
             }
-//            if (!commandEdt.text.isEmpty()) {
-//                Thread {
-//                    val socket = Socket(remoteIpEdt.text.toString(), Integer.parseInt(remotePortEdt.text.toString()))
-//                    val bos = BufferedOutputStream(socket.getOutputStream())
-//                    bos.write(localServerNameEdt.text.toString().toByteArray())
-//                    bos.flush()
-//                    bos.write(Constant.MESSAGE_HEAD_BYTES)
-//                    bos.write(commandEdt.text.toString().toByteArray())
-//                    bos.write(Constant.MESSAGE_END_BYTES)
-//                    bos.flush()
-//                    socket.close()
-//                    runOnUiThread{
-//                        Toast.makeText(this, "发送命令: " + commandEdt.text.toString(), Toast.LENGTH_SHORT).show()
-//                    }
-//                }.start()
-//            }
+            if (sendCommandCb.isChecked) {
+                if (!commandEdt.text.isEmpty()) {
+                    Thread {
+                        val socket =
+                            Socket(remoteIpEdt.text.toString(), Integer.parseInt(remotePortEdt.text.toString()))
+                        val bos = BufferedOutputStream(socket.getOutputStream())
+                        bos.write(localServerNameEdt.text.toString().toByteArray())
+                        bos.flush()
+                        bos.write(Constant.MESSAGE_HEAD_BYTES)
+                        bos.write(commandEdt.text.toString().toByteArray())
+                        bos.write(Constant.MESSAGE_END_BYTES)
+                        bos.flush()
+                        socket.close()
+                        runOnUiThread {
+                            Toast.makeText(this, "发送命令: " + commandEdt.text.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }.start()
+                } else {
+                    Toast.makeText(this, "命令为空", Toast.LENGTH_SHORT).show()
+                }
+            }
             if (sendFileCb.isChecked) {
                 if (isSelectFile) {
                     Thread {
-                        val socket = Socket(remoteIpEdt.text.toString(), Integer.parseInt(remotePortEdt.text.toString()))
+                        val socket =
+                            Socket(remoteIpEdt.text.toString(), Integer.parseInt(remotePortEdt.text.toString()))
                         val bos = BufferedOutputStream(socket.getOutputStream())
                         bos.write(localServerNameEdt.text.toString().toByteArray())
                         bos.flush()
@@ -74,11 +82,19 @@ class MainActivity : AppCompatActivity() {
                             Toast.makeText(this, "发送命令: " + commandEdt.text.toString(), Toast.LENGTH_SHORT).show()
                         }
                     }.start()
+                } else {
+                    Toast.makeText(this, "没有选中文件", Toast.LENGTH_SHORT).show()
                 }
             }
         }
+        clearSelectorBtn.setOnClickListener {
+            fileSelector.text = "点击选择文件"
+            clearSelectorBtn.visibility = View.GONE
+            isSelectFile = false
+        }
         fileSelector.setOnClickListener {
-            FilePickerManager.from(this).forResult(FilePickerManager.REQUEST_CODE)
+            //初始化fileSelector
+            FilePickerManager.from(this).maxSelectable(1).forResult(FilePickerManager.REQUEST_CODE)
         }
     }
 
@@ -92,6 +108,7 @@ class MainActivity : AppCompatActivity() {
                         isSelectFile = true
                         fileSelector.text = list.get(0)
                     }
+                    clearSelectorBtn.visibility = View.VISIBLE
                 } else {
                     Toast.makeText(this, "没有选择任何东西~", Toast.LENGTH_SHORT).show()
                 }
